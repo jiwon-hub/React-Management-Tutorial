bd5a6d03707f7554eb4b06169016983e9ab66ebc
you jiwon <yjw950810@gmail.com>
2020-11-02 15:17:25 +0900

    Create Customer.js

--- management/src/App.js
+++ management/src/App.js
@@ -1,30 +1,58 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer.js'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} let="logo"/>
-      <h2>Let's develop management system</h2>
-    </div>
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/any/1',
+  'name': '유지원',
+  'birthday': '950810',
+  'gender': '여자',
+  'job': '퍼블리셔'
+  },
+  {
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/any/2',
+  'name': '홍길동',
+  'birthday': '870923',
+  'gender': '남자',
+  'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/any/3',
+    'name': '이순신',
+    'birthday': '000000',
+    'gender': '남자',
+    'job': '장군'
+  },
+]
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        {
+          customers.map(c =>  {
+            return (
+              <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+            )
+          })
+        }
+
+      </div>
+    )
+  }
 }
 
 export default App;
